e881bfeac44ca8b86d0850073b003d538979ab5c
eilenee <eilenee@nate.com>
2016-11-14 23:01:00 +0900

    2nd chapter finished

---
+++ chapter 2/Variable.py
@@ -0,0 +1,51 @@
+Python 2.7.5 (default, May 15 2013, 22:43:36) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> Teacher = "Mr. Morton"
+>>> print Teacher
+Mr. Morton
+>>> print "53 + 28"
+53 + 28
+>>> print 53 + 28
+81
+>>> 5 + 3
+8
+>>> First = 5
+>>> Second = 3
+>>> print First + Second
+8
+>>> Third = First + Second
+>>> Third
+8
+>>> MyTeacher = "Mrs. Goodyear"
+>>> YourTeacher = MyTeacher
+>>> MyTeacher
+'Mrs. Goodyear'
+>>> YourTeacher
+'Mrs. Goodyear'
+>>> first = 5
+>>> second = 3
+>>> first + second
+8
+>>> first = '5'
+>>> second = '3'
+>>> first + second
+'53'
+>>> print "cat" + "dog"
+catdog
+>>> long_string = '''Sing a song of sixpence, a pocket full of rye,
+Four and twenty blackbirds baked in a pie.
+When the pie was opened the birds began to sing.
+Wasn't that a dainty dish to set before the king?'''
+>>> Teacher
+'Mr. Morton'
+>>> Teacher = 'Mr. Smith'
+>>> Teacher
+'Mr. Smith'
+>>> Score = 7
+>>> Score = Score
+>>> Score = Score + 1
+>>> print Score
+8
+>>> t = 'Mr. Morton'
+>>> t
+'Mr. Morton'
