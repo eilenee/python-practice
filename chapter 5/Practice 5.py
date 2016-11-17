71071897a2378b56649d520e5c84e6eb34b71b72
eilenee <eilenee@nate.com>
2016-11-17 23:47:03 +0900

    5th chapter finished

---
+++ chapter 5/Practice 5.py
@@ -0,0 +1,6 @@
+coins500 = int(raw_input('How many 500-won coins? '))
+coins100 = int(raw_input('How many 100-won coins? '))
+coins50 = int(raw_input('How many 50-won coins? '))
+coins10 = int(raw_input('How many 10-won coins? '))
+total = 500 * coins500 + 100 * coins100 + 50 * coins50 + 10 * coins10
+print "You have a total of: ", total
