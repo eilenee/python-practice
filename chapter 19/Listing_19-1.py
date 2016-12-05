f0eabee06c60282c13a347f0e26ab398f02bde10
eilenee <eilenee@nate.com>
2016-12-05 23:34:58 +0900

    19th chapter finished

---
+++ chapter 19/Listing_19-1.py
@@ -0,0 +1,16 @@
+import pygame, sys
+pygame.init()
+pygame.mixer.init()
+
+screen = pygame.display.set_mode([640,480])
+pygame.time.delay(1000)
+
+splat = pygame.mixer.Sound("splat.wav")
+splat.play()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
